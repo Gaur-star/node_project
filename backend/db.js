1871ba37d33cfcf28fa213fe6d6d9a9cb32fbe2e
Gaur-star <gaurav4star@gmail.com>
2026-09-12 18:51:18 +0530

    login

---
+++ backend/db.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "elphill123",
+  database: "node_project",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("MySQL connection failed:", err);
+    return;
+  }
+
+  console.log("MySQL connected!");
+});
+
+module.exports = db;
--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,45 @@ const express = require("express");
 const cors = require("cors");
 const PORT = 8100;
 const app = express();
+const db = require("./db");
 
 app.use(cors());
 app.use(express.json());
 
+
 app.post("/api/login", (req, res) => {
+
+
   const { email, password } = req.body;
 
   console.log("Email:", email);
   console.log("Password:", password);
 
+  
+  /******save to db ***********/
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const sql = `
+      INSERT INTO users (email, password)
+      VALUES (?, ?)
+    `;
+
+    db.query(sql, [email, hashedPassword], (err, result) => {
+      if (err) {
+        console.error("Database error:", err);
+        return res.status(500).json({
+          message: "Could not save user",
+        });
+      }
+
+      console.log("User saved! ID:", result.insertId);
+
+  /******save to db ***********/
+
   res.json({
     message: "Data received successfully",
   });
-  
+    });
 });
 
 app.get("/", (req,res) => {
