9c59969347d1f58d6d4845e97baa59b82db88c9b
Gaur-star <gaurav4star@gmail.com>
2026-09-12 16:07:36 +0530

    loginPage

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,19 @@ const PORT = 8100;
 
 const app = express();
 
+
+app.post("/api/login", (req, res) => {
+  const { email, password } = req.body;
+
+  console.log("Email:", email);
+  console.log("Password:", password);
+
+  res.json({
+    message: "Data received successfully",
+  });
+  
+});
+
 app.get("/", (req,res) => {
     res.send("HI");
 } );
--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -10,8 +10,36 @@ function Login() {
   function handleLogin(e) {
     e.preventDefault();
 
-    console.log("Email:", email);
-    console.log("Password:", password);
+    // console.log("Email:", email);
+    // console.log("Password:", password);
+
+    try {
+    const response = await fetch("http://localhost:5000/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      alert(data.message);
+      return;
+    }
+
+    console.log("Backend response:", data);
+    alert("Login successful!");
+
+    } catch (error) {
+      console.error("Login error:", error);
+      alert("Cannot connect to backend");
+    }
+
   }
 
   return (
