2ede4f5619a692fc39c4ec2c7f07858f43170e43
Gaur-star <gaurav4star@gmail.com>
2026-09-14 18:45:58 +0530

    webpage

--- frontend/src/components/dashboard.jsx
+++ frontend/src/components/dashboard.jsx
@@ -1,3 +1,10 @@
+import { Outlet } from "react-router-dom";
+
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+
 function Web(){
     return(
         <h1>Hello</h1>      
