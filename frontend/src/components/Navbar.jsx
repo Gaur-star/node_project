409054c177ab725c2edf4cf6986663ae107c512a
Gaur-star <gaurav4star@gmail.com>
2026-09-26 16:51:15 +0530

    frontend

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (
@@ -17,41 +18,45 @@ function Navbar() {
                 </div>
                 </div>
             </div>
+
             <div className="container px-0">
                 <nav className="navbar navbar-light bg-white navbar-expand-xl">
-                <a href="index.html" className="navbar-brand"><h1 className="text-primary display-6">Fruitables</h1></a>
+                <Link to="/index" className="navbar-brand"><h1 className="text-primary display-6">Fruitables</h1></Link>
                 <button className="navbar-toggler py-2 px-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                     <span className="fa fa-bars text-primary" />
                 </button>
                 <div className="collapse navbar-collapse bg-white" id="navbarCollapse">
                     <div className="navbar-nav mx-auto">
-                    <a href="index.html" className="nav-item nav-link active">Home</a>
-                    <a href="shop.html" className="nav-item nav-link">Shop</a>
-                    <a href="shop-detail.html" className="nav-item nav-link">Shop Detail</a>
+                    <NavLink to="/index" className="nav-item nav-link active">Home</NavLink>
+                    <NavLink to="/shop" className="nav-item nav-link">Shop</NavLink>
+                    <NavLink to="/shop-detail" className="nav-item nav-link">Shop Detail</NavLink>
+
                     <div className="nav-item dropdown">
-                        <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                        <Link to="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</Link>
                         <div className="dropdown-menu m-0 bg-secondary rounded-0">
-                        <a href="cart.html" className="dropdown-item">Cart</a>
-                        <a href="chackout.html" className="dropdown-item">Chackout</a>
-                        <a href="testimonial.html" className="dropdown-item">Testimonial</a>
-                        <a href="404.html" className="dropdown-item">404 Page</a>
+                        <Link to="/cart" className="dropdown-item">Cart</Link>
+                        <Link to="/checkout" className="dropdown-item">Checkout</Link>
+                        <Link to="/testimonial" className="dropdown-item">Testimonial</Link>
+                        <Link to="/404" className="dropdown-item">404 Page</Link>
                         </div>
                     </div>
-                    <a href="contact.html" className="nav-item nav-link">Contact</a>
+
+                    <NavLink to="/contact" className="nav-item nav-link">Contact</NavLink>
                     </div>
                     <div className="d-flex m-3 me-0">
                     <button className="btn-search btn border border-secondary btn-md-square rounded-circle bg-white me-4" data-bs-toggle="modal" data-bs-target="#searchModal"><i className="fas fa-search text-primary" /></button>
-                    <a href="#" className="position-relative me-4 my-auto">
+                    <Link to="#" className="position-relative me-4 my-auto">
                         <i className="fa fa-shopping-bag fa-2x" />
                         <span className="position-absolute bg-secondary rounded-circle d-flex align-items-center justify-content-center text-dark px-1" style={{top: '-5px', left: 15, height: 20, minWidth: 20}}>3</span>
-                    </a>
-                    <a href="#" className="my-auto">
+                    </Link>
+                    <Link to="#" className="my-auto">
                         <i className="fas fa-user fa-2x" />
-                    </a>
+                    </Link>
                     </div>
                 </div>
                 </nav>
             </div>
+
         </div>
 
     </div>
